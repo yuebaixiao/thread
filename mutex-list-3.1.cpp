055f7635b047ecc70e665780309b6b51daecacf8
yuebaixiao <wsyueshuai@163.com>
2018-10-30 22:59:57 +0800

    mutex-list

---
+++ mutex-list-3.1.cpp
@@ -0,0 +1,45 @@
+#include <list>
+#include <iostream>
+#include <mutex>
+#include <algorithm>
+#include <thread>
+#include <unistd.h>
+
+using namespace std;
+
+class data_protect{
+public:
+  list<int> alist{1,2};
+  mutex m;
+public:
+  
+  void add_list(int val){
+    lock_guard<mutex> g(m);
+    alist.push_back(val);
+  }
+  bool contains(int val){
+    lock_guard<mutex> g(m);
+    return find(alist.begin(), alist.end(), val) != alist.end();
+  }
+};
+
+void func(data_protect& dp){
+  dp.add_list(12);
+}
+
+int main(){
+  data_protect dp;
+  thread t(func, ref(dp));
+  //t.join();
+  t.detach();
+  //sleep(1);
+  dp.add_list(12);
+  if(dp.contains(12)){
+    cout << "contains 12" << endl;
+  }
+  for(auto& s : dp.alist){
+    cout << s << endl;
+  }
+  pthread_exit(NULL);
+
+}
