3fd16472accb9eaee0cb736e54f8bd0b8bbf7dca
yuebaixiao <wsyueshuai@163.com>
2018-11-22 16:19:59 +0800

    futrue-return

--- future-return.4.6.cpp
+++ future-return.4.6.cpp
@@ -4,7 +4,7 @@
 
 int return_from_thread(int val){
   std::cout << val << std::endl;
-  sleep(1);
+  //sleep(1);
   return val;
 }
 void do_something(){
@@ -12,8 +12,11 @@ void do_something(){
 }
 
 int main(){
+  //std::future<int> ret = std::async(std::launch::deferred,return_from_thread, 10);
+  //std::future<int> ret = std::async(std::launch::async,return_from_thread, 10);
   std::future<int> ret = std::async(return_from_thread, 10);
   do_something();
+  //ret.wait();
   std::cout << "车来了，车牌号：" << ret.get() << std::endl;
   std::cout << "end" << std::endl;
 }
