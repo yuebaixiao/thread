00979d67af03b6578002736703d9c643d092f17e
yuebaixiao <wsyueshuai@163.com>
2018-11-24 23:49:57 +0800

    packaged_task

--- packaged_task-4.9.cpp
+++ packaged_task-4.9.cpp
@@ -4,6 +4,7 @@
 #include <thread>
 #include <iostream>
 #include <unistd.h>
+#include <string>
 //#include <utility>
 
 std::mutex mut;
@@ -11,7 +12,7 @@ std::deque<std::packaged_task<std::string(int)>> tasks;
 
 void manage_tasks(){
   while(true){
-    sleep(2);
+    sleep(1);
     //std::cout << "please wait for a moument" << std::endl;
     std::packaged_task<std::string(int)> task;
     {
@@ -36,7 +37,7 @@ std::future<std::string> add_task(Call ca){
   return ret;
 }
 
-std::string jiaoche(int i = 0){
+std::string call_texi(int i = 0){
   std::cout << "-------------jiaoche---------------" << std::endl;
   if(i == 1){
     return "aaa";
@@ -44,19 +45,21 @@ std::string jiaoche(int i = 0){
     return "bbb";
   }
 }
-int main(){
 
+std::string call_zhuanche(int i){
+  std::cout << "zhuanche:" << i << std::endl;
+  return std::to_string(i);
+}
+int main(){
+  
   std::thread background_thread(manage_tasks);
   background_thread.detach();
 
-
-  std::future<std::string> fut1 = add_task(jiaoche);
-  //std::string s1 = fut1.get();
-  //std::cout << s1 << std::endl;
+  std::future<std::string> fut1 = add_task(call_texi);
+  std::cout << fut1.get() << std::endl;
   
-  std::future<std::string> fut2 = add_task(jiaoche);
-  //std::string s2 = fut2.get();
-  //std::cout << s2 << std::endl;
+  std::future<std::string> fut2 = add_task(call_zhuanche);
+  std::cout << fut2.get() << std::endl;
 
   pthread_exit(NULL);
 
