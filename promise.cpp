ee4901dd1e13828e5c90cc9784e7c31f28a18b3f
yuebaixiao <wsyueshuai@163.com>
2018-11-25 17:05:24 +0800

    promise

---
+++ promise.cpp
@@ -0,0 +1,17 @@
+#include <future>
+#include <thread>
+#include <iostream>
+
+void thread1(std::promise<int> p, int val){
+  std::cout << "in thread1" << std::endl;
+  p.set_value(val);
+}
+int main(){
+  std::promise<int> p;
+  std::future<int> f = p.get_future();
+  std::thread t1(thread1, std::move(p), 10);
+  t1.detach();
+
+  std::cout << f.get() << std::endl;
+  pthread_exit(NULL);
+}
