b1f7057df05cb646d0d358e53bcc9ab3ab30fdbd
yuebaixiao <wsyueshuai@163.com>
2018-12-04 14:43:32 +0800

    copy_constructor

--- const/001.cpp
+++ const/001.cpp
@@ -1,27 +1,44 @@
 #include <iostream>
 
-class test{
-  int data;
+class Int{
+private:
+  int mi;
 public:
-  test(int d = 0) : data(d){}
+  //Int(){}
+  
+  Int(int i):mi(i){
+    std::cout << "c" << std::endl;
+  }
+  Int(const Int& tmp){
+    mi = tmp.mi;
+  }
+  
+  ~Int(){}
+};
+
+class test{
+  Int data;
+public:
+  test(Int d) : data(d){}
+  test(const test& t){
+    //data = t.data;
+  }
   ~test(){}
-  int getvalue(){
+  Int getvalue(){
     return data;
   }
-  int getvalue() const {
+  //重载方法
+  Int getvalue() const {
     return data;
-  }
-  void setvalue const (int d){
-    data = d;
   }
 };
 
 int main(){
-  test t1(19);
-  const test t2(12);
-
-  t1.setvalue(11);
+  //Int d1(10);
+  //test t1(10);
+  //const test t2(12);
   
-  int a1 = t1.getvalue();
-  const int& b1 = t1.getvalue();
+  //Int a1 = t2.getvalue();
+  //int& b1 = t2.getvalue();
+  //const Int& c1 = t2.getvalue();
 }
