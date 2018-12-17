4a861df9e91245495ecd5d390b453ce073be6740
yuebaixiao <wsyueshuai@163.com>
2018-12-17 08:46:04 +0800

    StrBolb

---
+++ operator/StrBlob.cpp
@@ -0,0 +1,60 @@
+#include "StrBlob.h"
+#include <iostream>
+
+StrBlob::StrBlob() : data(std::make_shared<std::vector<std::string>>()){}
+StrBlob::StrBlob(std::initializer_list<std::string> il) :
+  data(std::make_shared<std::vector<std::string>>(il)){}
+
+void StrBlob::check(size_type i, const std::string& msg)const{
+  if(i >= data->size()){
+    throw std::out_of_range(msg);
+  }
+}
+
+std::string& StrBlob::front(){
+  check(0, "front");
+  return data->front();
+}
+
+std::string& StrBlob::back(){
+  check(0, "back");
+  return data->back();
+}
+
+void StrBlob::pop_back(){
+  check(0, "pop_back");
+  data->pop_back();
+}
+bool operator==(const StrBlob& lhs, const StrBlob& rhs){
+  /*
+  if(lhs.data->size() >=0 && lhs.data->size() == rhs.data->size()){
+    for(int i = 0; i < lhs.data->size(); ++i){
+      if((*lhs.data)[i] != (*rhs.data)[i]){
+	return false;
+      }
+    }
+    return true;
+  }
+  else{
+    return false;
+  }
+  */
+  return *lhs.data == *rhs.data;
+  
+}
+bool operator!=(const StrBlob& lhs, const StrBlob& rhs){
+  return !operator==(lhs, rhs);
+}
+  
+int main(){
+  StrBlob b1 = {"aa","bb"};
+  StrBlob b2 = {"aa","bb"};
+  StrBlob b3 = {"aa","bb","dd"};
+  using namespace std;
+  if(b1 == b2){
+    cout << "true" << endl;
+  }
+  if(b1 != b3){
+    cout << "false" << endl;
+  }
+}
---
+++ operator/StrBlob.h
@@ -0,0 +1,23 @@
+#include <memory>
+#include <string>
+#include <vector>
+
+class StrBlob{
+  friend bool operator==(const StrBlob&, const StrBlob&);
+  friend bool operator!=(const StrBlob&, const StrBlob&);
+ public:
+  typedef std::vector<std::string>::size_type size_type;
+  StrBlob();
+  StrBlob(std::initializer_list<std::string>);
+  size_type size() const{return data->size();}
+  bool empty()const {return data->empty();}
+  void push_back(const std::string& t){data->push_back(t);}
+  void pop_back();
+  std::string& front();
+  std::string& back();
+ private:
+  std::shared_ptr<std::vector<std::string>> data;
+  void check(size_type, const std::string&) const;
+};
+bool operator==(const StrBlob&, const StrBlob&);
+bool operator!=(const StrBlob&, const StrBlob&);
