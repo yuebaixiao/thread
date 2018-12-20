0a7f70b2253496b179553e3a18f6411994a32d0e
yuebaixiao <wsyueshuai@163.com>
2018-12-20 22:22:34 +0800

    String

--- copy_control/String.cpp
+++ copy_control/String.cpp
@@ -1,7 +1,12 @@
 #include "String.h"
 
 String::String(const char* p){
-
+  char* tmp = (char*)p;
+  while(*tmp++ != '\0'){
+  }
+  auto pa = alloc_n_copy(p, tmp);
+  elements = pa.first;
+  first_free = cap = pa.second;
 }
 
 std::pair<char*, char*> String::alloc_n_copy
@@ -10,4 +15,41 @@ std::pair<char*, char*> String::alloc_n_copy
   return {data, std::uninitialized_copy(b, e, data)};
 }
 
-int main(){}
+std::ostream& operator<<(std::ostream& os, const String& str){
+  for(char* tmp = str.elements; tmp != str.first_free;++tmp){
+    os << *tmp;
+  }
+  return os;
+}
+
+bool operator==(const String& lhs, const String& rhs){
+  if(lhs.size() == rhs.size()){
+    char* t1 = lhs.elements;
+    char* t2 = rhs.elements;
+    while(*t1 == *t2 && *t1 != '\0'){
+      t1++;
+      t2++;
+    }
+    if(*t1 == '\0'){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  else{
+    return false;
+  }
+}
+
+int main(){
+  String s1("qq");
+  String s2("qq");
+  if(s1 == s2){
+    std::cout << "true" << std::endl;
+  }
+  else{
+    std::cout << "false" << std::endl;
+  }
+  std::cout << s1 << std::endl;
+}
--- copy_control/String.h
+++ copy_control/String.h
@@ -1,15 +1,25 @@
 #include <memory>
+#include <iostream>
 
 class String{
+  friend std::ostream& operator<<(std::ostream&, const String&);
+  friend bool operator==(const String&, const String&);
+  friend bool operator!=(const String&, const String&);
  public:
   String(const char*);
+  size_t size() const{
+    return first_free - elements;
+  }
  private:
   std::pair<char*, char*> alloc_n_copy
   (const char* b, const char* e);
   static std::allocator<char> alloc;
-  std::string* elements;
-  std::string* first_free;
-  std::string* cap;
+  char* elements;
+  char* first_free;
+  char* cap;
 };
 
 std::allocator<char> String::alloc;
+std::ostream& operator<<(std::ostream&, const String&);
+bool operator==(const String&, const String&);
+bool operator!=(const String&, const String&);
