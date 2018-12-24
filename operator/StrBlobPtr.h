7c254a6fc3e9bcc908a54ed38d2e8676a5a913ee
yuebaixiao <wsyueshuai@163.com>
2018-12-24 22:30:38 +0800

    up

--- operator/StrBlobPtr.h
+++ operator/StrBlobPtr.h
@@ -11,14 +11,27 @@ class StrBlobPtr{
  public:
   StrBlobPtr() : curr(0){}
   StrBlobPtr(StrBlob& a, size_t sz = 0):wptr(a.data), curr(sz){}
+
+  //方法get和重载*的效果是一样的
   std::string get(){
     auto ptr = check(curr, "get string value");
     return (*ptr)[curr];
   }
+  
+  //方法get和重载*的效果是一样的
+  std::string& operator*(){
+    auto p = check(curr, "get string value");
+    return (*p)[curr];
+  }
+  std::string* operator->(){
+    return & this->operator*();
+  }
+  
   StrBlobPtr& operator++();
   StrBlobPtr& operator--();
   StrBlobPtr operator++(int);
   StrBlobPtr operator--(int);
+
  private:
   std::shared_ptr<std::vector<std::string>>
     check(std::size_t, const std::string&) const;
--- operator/mainStrBlobPtr.cpp
+++ operator/mainStrBlobPtr.cpp
@@ -1,5 +1,14 @@
+#include "StrBlob.h"
 #include "StrBlobPtr.h"
+#include <iostream>
 
+using namespace std;
 int main(){
   StrBlob s1{"11", "22"};
+  StrBlobPtr p1 = s1.begin();
+  StrBlobPtr tm = ++p1;
+  cout << tm->size() << endl;
+  p1--;
+  tm = p1;
+  cout << *tm << endl;
 }
