cf3bc9177aae35eccea28880bc89d88e7b151340
yuebaixiao <wsyueshuai@163.com>
2018-12-27 18:43:59 +0800

    Quote

--- oop/Quote.h
+++ oop/Quote.h
@@ -7,7 +7,7 @@ class Quote{
     :bookNo(book), price(pri){}
   std::string isbn() const{return bookNo;}
   virtual double net_price(std::size_t n)const{
-    return n * price
+    return n * price;
   }
   virtual ~Quote() = default;
  private:
@@ -27,9 +27,24 @@ class Bulk_quote : public Quote{
   double discount = 0.0;//折扣额
 };
 
-double print_total(ostream& os,
+
+Bulk_quote::Bulk_quote(const std::string& book, double p,
+		       std::size_t qty, double disc):
+  Quote(book, p), min_qty(qty), discount(disc){}
+
+double Bulk_quote::net_price(std::size_t cnt)const {
+  if(cnt >= min_qty){
+    return cnt * (1 - discount) * price;
+  }
+  else{
+    return cnt * price;
+  }
+}
+
+
+double print_total(std::ostream& os,
 		   const Quote& item, size_t n){
-  double ret = item.net_price();
+  double ret = item.net_price(n);
   os << "ISBN: " << item.isbn()
      << " # sold: " << n << " total due: " << ret << std::endl;
   return ret;
---
+++ oop/mainQuote.cpp
@@ -0,0 +1,6 @@
+#include "Quote.h"
+
+int main(){
+  Quote q("01",100.5);
+  Bulk_quote bq("01", 100.5, 5, 1);
+}
