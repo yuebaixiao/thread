eac7b6d05ae4d601da4f2dad4eaa3f35c617b99e
yuebaixiao <wsyueshuai@163.com>
2018-12-28 17:12:39 +0800

    quote

--- oop/Quote.h
+++ oop/Quote.h
@@ -1,3 +1,6 @@
+#ifndef __QUOTE_H__
+#define __QUOTE_H__
+
 #include <iostream>
 
 class Quote{
@@ -28,24 +31,4 @@ class Bulk_quote : public Quote{
 };
 
 
-Bulk_quote::Bulk_quote(const std::string& book, double p,
-		       std::size_t qty, double disc):
-  Quote(book, p), min_qty(qty), discount(disc){}
-
-double Bulk_quote::net_price(std::size_t cnt)const {
-  if(cnt >= min_qty){
-    return cnt * (1 - discount) * price;
-  }
-  else{
-    return cnt * price;
-  }
-}
-
-
-double print_total(std::ostream& os,
-		   const Quote& item, size_t n){
-  double ret = item.net_price(n);
-  os << "ISBN: " << item.isbn()
-     << " # sold: " << n << " total due: " << ret << std::endl;
-  return ret;
-}
+#endif
--- oop/mainQuote.cpp
+++ oop/mainQuote.cpp
@@ -1,6 +1,10 @@
 #include "Quote.h"
 
+double print_total(std::ostream& os,
+		   const Quote& item, size_t n);
 int main(){
   Quote q("01",100.5);
+  double d = print_total(std::cout, q, 1);
+
   Bulk_quote bq("01", 100.5, 5, 1);
 }
